18745641c96ded8044c2e679d54d237bc2054611
Cody Reading <codyreading@gmail.com>
2020-11-10 17:38:31 -0500

    Updated eval

--- create_waymo_eval_bin.py
+++ create_waymo_eval_bin.py
@@ -13,66 +13,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================*/
-"""A simple example to generate a file that contains serialized Objects proto."""
+"""Generates a files that contains serialized Objects proto for ground truth and predictions."""
 
 from waymo_open_dataset import dataset_pb2
 from waymo_open_dataset import label_pb2
 from waymo_open_dataset.protos import metrics_pb2
 
-import pickle
+import argparse
 import os
-import pdb
+import numpy as np
+import math
 
 
-def _create_pd_file_example():
-    """Creates a prediction objects file."""
-    objects = metrics_pb2.Objects()
-
-    o = metrics_pb2.Object()
-    # The following 3 fields are used to uniquely identify a frame a prediction
-    # is predicted at. Make sure you set them to values exactly the same as what
-    # we provided in the raw data. Otherwise your prediction is considered as a
-    # false negative.
-    o.context_name = ('context_name for the prediction. See Frame::context::name '
-                      'in  dataset.proto.')
-    # The frame timestamp for the prediction. See Frame::timestamp_micros in
-    # dataset.proto.
-    invalid_ts = -1
-    o.frame_timestamp_micros = invalid_ts
-    # This is only needed for 2D detection or tracking tasks.
-    # Set it to the camera name the prediction is for.
-    o.camera_name = dataset_pb2.CameraName.FRONT
-
-    # Populating box and score.
-    box = label_pb2.Label.Box()
-    box.center_x = 0
-    box.center_y = 0
-    box.center_z = 0
-    box.length = 0
-    box.width = 0
-    box.height = 0
-    box.heading = 0
-    o.object.box.CopyFrom(box)
-    # This must be within [0.0, 1.0]. It is better to filter those boxes with
-    # small scores to speed up metrics computation.
-    o.score = 1
-    # For tracking, this must be set and it must be unique for each tracked
-    # sequence.
-    o.object.id = 'unique object tracking ID'
-    # Use correct type.
-    pdb.set_trace()
-    o.object.type = label_pb2.Label.TYPE_PEDESTRIAN
-
-    objects.objects.append(o)
-
-    # Add more objects. Note that a reasonable detector should limit its maximum
-    # number of boxes predicted per frame. A reasonable value is around 400. A
-    # huge number of boxes can slow down metrics computation.
-
-    # Write objects to a file.
-    f = open('/tmp/your_preds.bin', 'wb')
-    f.write(objects.SerializeToString())
-    f.close()
+def sigmoid(x):
+    return math.exp(-np.logaddexp(0, -x))
 
 
 def save_preds():
@@ -115,72 +69,121 @@ def save_preds():
     f.close()
 
 
-def save_gt():
-    gt_infos = pickle.load(open('waymo_infos_val_v5_80.pkl', "rb"))
+class Object3d(object):
+
+    def __init__(self, line, type_):
+        self.label = line.strip().split(' ')
+        self.src = line
+        self.cls_type = self.label[0]
+        self.truncation = float(self.label[1])
+        self.occlusion = float(self.label[2])  # 0:fully visible 1:partly occluded 2:largely occluded 3:unknown
+        self.alpha = float(self.label[3])
+        self.box2d = np.array((float(self.label[4]),
+                               float(self.label[5]),
+                               float(self.label[6]),
+                               float(self.label[7])), dtype=np.float32)
+        self.h = float(self.label[8])
+        self.w = float(self.label[9])
+        self.l = float(self.label[10])
+        self.loc = np.array((float(self.label[11]), float(self.label[12]), float(self.label[13])), dtype=np.float32)
+        self.dis_to_cam = np.linalg.norm(self.loc)
+        self.ry = float(self.label[14])
+        self.level_str = None
+
+        self.num_pts = -1.0
+        self.score = 1.0
+
+        if type_ == "preds":
+            self.score = sigmoid(float(self.label[15]))
+        elif type_ == "gt":
+            self.num_pts = int(self.label[15])
+            self.prepopulated_level = int(self.label[16])
+        else:
+            raise NotImplementedError
+
+        self.prepopulated_level = 1.0
+        self.level = self.get_obj_level()
+
+    def get_obj_level(self):
+        if self.prepopulated_level == 2:
+            self.level_str = 'LEVEL_2'
+            return 2
+        elif self.num_pts <= 5 and self.num_pts > 0:
+            self.level_str = 'LEVEL_2'
+            return 2
+        elif self.num_pts > 5:
+            self.level_str = 'LEVEL_1'
+            return 1
+        elif self.num_pts == 0:
+            self.level_str = 'Ignore'
+            return 0
+        else:
+            self.level_str = 'UnKnown'
+            return -1
+
+
+def get_objects_from_file(file, type_):
+    with open(file, "r") as f:
+        lines = f.readlines()
+    objects = [Object3d(line, type_) for line in lines]
+    return objects
+
+
+def create_bin(input_dir, output_dir, type_):
+
+    samples = sorted(os.listdir(input_dir))
     objects = metrics_pb2.Objects()
-    for sample in gt_infos:
-        calib = sample['calib']
-        annos = sample['annos']
-        gt_boxes = annos['gt_boxes_lidar']
-        names = annos['name']
-        num_boxes = len(gt_boxes)
-        for i in range(0, num_boxes):
-            gt_box = gt_boxes[i]  # [x, y, z, w, l, h, rz, gt_classes]
-            name = names[i]
+
+    for sample in samples:
+
+        sample_file = os.path.join(input_dir, sample)
+        objects = get_objects_from_file(sample_file, type_)
+
+        for obj in objects:
             o = metrics_pb2.Object()
-            o.context_name = (calib['Name'])
-            o.frame_timestamp_micros = calib['Timestamp']
+            o.context_name = ('')
+            o.frame_timestamp_micros = -1
 
             # Populating box and score.
             box = label_pb2.Label.Box()
-            box.center_x = gt_box[0]
-            box.center_y = gt_box[1]
-            box.center_z = gt_box[2]
-            box.width = gt_box[3]
-            box.length = gt_box[4]
-            box.height = gt_box[5]
-            box.heading = gt_box[6]
-            o.score = 0.5
+            box.center_x = obj.loc[0]
+            box.center_y = obj.loc[1]
+            box.center_z = obj.loc[2]
+
+            # Check these
+            box.length = obj.l
+            box.width = obj.h
+            box.height = obj.w
+
+            box.heading = obj.ry
             o.object.box.CopyFrom(box)
-            o.object.detection_difficulty_level = annos['difficulty'][i]
+
+            if type_ == "gt":
+                o.object.detection_difficulty_level = getattr(label_pb2.Label, obj.level_str)
+                # Add num pts
+                o.object.num_lidar_points_in_box = obj.num_pts
+
+            o.score = obj.score
+
             # Use correct type
-            class_name = 'TYPE_' + name
+            class_name = 'TYPE_' + obj.cls_type
             o.object.type = getattr(label_pb2.Label, class_name)
             objects.objects.append(o)
-    f = open('val.bin', 'wb')
-    f.write(objects.SerializeToString())
-    f.close()
+
+    output_file = os.path.join(output_dir, "{}.bin".format(type_))
+    with open(output_file, 'wb') as f:
+        f.write(objects.SerializeToString())
 
 
-def read_bin():
-    f = open('official_val.bin', 'rb')
-    content = f.read()
-    official_gt = metrics_pb2.Objects()
-    official_gt.ParseFromString(content)
-    f.close()
-    f = open('val.bin', 'rb')
-    content2 = f.read()
-    gt = metrics_pb2.Objects()
-    gt.ParseFromString(content2)
-    f = open('fake_ground_truths.bin', 'rb')
-    content3 = f.read()
-    objects3 = metrics_pb2.Objects()
-    objects3.ParseFromString(content3)
-    f = open('fake_predictions.bin', 'rb')
-    content4 = f.read()
-    objects4 = metrics_pb2.Objects()
-    objects4.ParseFromString(content4)
-    counter = 0
-    counter_all = 0
-    pdb.set_trace()
-
-
-def main():
-    #_create_pd_file_example()
-    # read_bin()
-    save_preds()
-    save_gt()
+def main(args):
+    create_bin(input_dir=args.preds, output_dir=args.output_dir, type_="preds", )
+    create_bin(input_dir=args.gt, output_dir=args.output_dir, type_='gt')
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Generaate image sets')
+    parser.add_argument('--preds', type=str, default=None, help='Path to prediction text files')
+    parser.add_argument('--gt', type=str, default=None, help='Path to ground truth text files')
+    parser.add_argument('--output_dir', type=str, default=None, help='Path to output folder')
+    args = parser.parse_args()
+    main(args)
